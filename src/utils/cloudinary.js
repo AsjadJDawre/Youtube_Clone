c22fd7b30b0e442b3dfc4ad2ad20aa6c6d30197d
Asjad Johar Dawre <dawreasjad72@gmail.com>
2024-08-06 20:17:09 +0530

    cloudinary for File handling

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,2 +1,32 @@
 import {v2 as cloudinary} from "cloudinary";
 import fs from "fs"
+cloudinary.config({
+    cloud_name:process.env.CLOUD_NAME,
+    api_key: process.env.CLOUDNARY_API_KEY,
+    api_secret:process.env.CLOUDNARY_API_SECRET
+})
+
+const uploadOnCloudinary=async (localFilePath)=>{
+
+    try {
+        if(!localFilePath)return null ;
+        // upload the file on cloudinary 
+       const response = await  cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        // file has been uploaded Successfully 
+        console.log("file is uploaded on Cloudinary : ",response.url);
+        return response;
+        
+    } catch (error) {
+        // to Unlink file from Server OR  remove the locally saved temp file as the upload operation got failed!
+         fs.unlinkSync(localFilePath)
+         return null
+        
+    }
+
+}
+
+
+cloudinary.v2.uploader.upload("xyx",{public_id:"img"},(err,result)=>{console.log(result);;
+})
