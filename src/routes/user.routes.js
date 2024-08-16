4c6fce63181697fb85e2135a67ee429b3e0ca78a
Asjad Johar Dawre <dawreasjad72@gmail.com>
2024-08-16 22:28:17 +0530

    feat(auth): add method to generate new access token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,23 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logOutuser, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
 const router =Router();
+import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-router.route("/register").post(registerUser)
+router.route("/register").post(upload.fields([
+    {name:"avatar",maxCount:1}
+    ,{
+name:"coverImage",
+maxCount:1
+    }
+
+]),registerUser)
+
+router.route('/login').post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,logOutuser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 
 export default router
