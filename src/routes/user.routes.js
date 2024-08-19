3add4bd0a7ec6a800231c6ac26acbd8110351b15
Asjad Johar Dawre <dawreasjad72@gmail.com>
2024-08-19 07:21:53 +0530

    Add user account, profile, and media routes with JWT auth.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logOutuser, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logOutuser, registerUser,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 const router =Router();
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -18,6 +18,16 @@ router.route('/login').post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT,logOutuser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
 
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar",updateUserAvatar))
+router.route("/coverImage").patch(verifyJWT,upload.single("coverImage",updateCoverImage))
+
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/history").get(verifyJWT,getWatchHistory)
 
 export default router
